63c8049e0b1809d63df6d4d691eaaff746092fb6
justinavickers <justinav38@gmail.com>
2019-02-04 15:31:27 -0600

    added cars json file

---
+++ carDealership/main.js
@@ -0,0 +1,6 @@
+// Total profit for 2017
+// In which month did they sell the most cars?
+// Which salesperson sold the most cars?
+// Which salesperson made the most profit?
+// Which model was the most popular?
+// Which bank provided the most loans to our customers?
