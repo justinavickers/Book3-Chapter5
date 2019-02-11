5d05d16c1b7974f5e122ed3e760fc9a3ea7d509e
justinavickers <justinav38@gmail.com>
2019-02-10 19:36:55 -0600

    got the month for the most sales

--- carDealership/main.js
+++ carDealership/main.js
@@ -7,20 +7,28 @@ const sales = require("./cars")
 // Which bank provided the most loans to our customers?
 const filteredSales = sales.filter((sale) => new Date(sale.purchase_date).getFullYear() === 2017)
 let totalProfits = 0
-let monthSales = [0,0,0,0,0,0,0,0,0,0,0,0]
+let monthSales = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 const months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-filteredSales.forEach((sale)=>{
-  const {gross_profit, purchase_date} = sale;
+filteredSales.forEach((sale) => {
+  const { gross_profit, purchase_date } = sale;
   totalProfits += gross_profit;
 
   const month = new Date(purchase_date).getMonth()
   monthSales[month] += gross_profit
 })
 
-const maxMonthSales = monthSales.reduce((a,b,c) => {
-  return Math.max(a,b)
-})
+let currentMaxMonth = 0
+let maxProfit = 0
+
+ monthSales.forEach((profit, index) => {
+  if (profit > maxProfit) {
+    maxProfit = profit
+    currentMaxMonth = index
+  }
+});
+
+
 console.log("Total profit for 2017: ", totalProfits)
-console.log("In which month did they sell the most cars?", maxMonthSales)
+console.log("In which month did they sell the most cars?", months[currentMaxMonth])
 
